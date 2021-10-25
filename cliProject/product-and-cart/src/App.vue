e602cd1c6b547d8def09e05fce1313853b667c74
lucas mira <lucas-mira1@hotmail.com>
2021-10-25 20:46:31 -0300

    top nav

--- cliProject/product-and-cart/src/App.vue
+++ cliProject/product-and-cart/src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <!-- <router-link href="#" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart (0)</span>
+    </router-link> -->
+  </header>
+
+  <router-view />
 </template>
 
 <style>
